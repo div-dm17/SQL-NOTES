b7b76b6de39792ad357983a0a6f0147f29a9dccd
Divyanshu Mishra <37334916+div-dm17@users.noreply.github.com>
2021-03-31 11:20:06 +0530

    use DISTINCT
    Practice on book data using DISTINCT and ORDER BY

---
+++ BOOK./orderDistinct.sql
@@ -0,0 +1,15 @@
+
+
+SELECT title FROM books;
+
+SELECT DISTINCT author_fname, author_lname FROM books;
+ SELECT DISTINCT author_lname FROM books;
+ 
+ SELECT author_fname, author_lname FROM books ORDER BY author_lname, author_fname;
+
+SELECT title , released_year FROM books ;
+  SELECT DISTINCT title , released_year FROM books ORDER BY released_year  DESC LIMIT 5;
+
+ 
+ 
+ 
