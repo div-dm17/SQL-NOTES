9495e1a59c8eb0d6fd5e66166f2697255aa3a6a9
Divyanshu Mishra <37334916+div-dm17@users.noreply.github.com>
2021-03-31 16:49:29 +0530

    Add Group
    use with count, Concat

--- BOOK./count.sql
+++ BOOK./count.sql
@@ -9,3 +9,15 @@
  SELECT COUNT(DISTINCT author_lname, author_fname) FROM books;
  -- how many title contain the?
  SELECT COUNT(DISTINCT title) FROM books WHERE title LIKE '%the%';
+
+
+---ADD GROUP BY
+
+ 
+ SELECT * FROM books;
+  SELECT title, author_lname FROM books GROUP BY author_lname;
+ SELECT author_lname, COUNT(*)  FROM books GROUP BY author_lname;
+ SELECT author_fname, author_lname , COUNT(*) FROM books GROUP BY author_lname, author_fname;
+ SELECT CONCAT('In ',released_year, COUNT(*), ' ','book released') AS 'year' FROM books GROUP BY author_fname, author_lname;
+ 
+ 
