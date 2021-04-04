f0956a86b4fa82f117c436097d008818a2e1182e
Divyanshu Mishra <37334916+div-dm17@users.noreply.github.com>
2021-04-04 21:24:46 +0530

    Add AND
    And operator

--- BOOK./Logical.sql
+++ BOOK./Logical.sql
@@ -9,3 +9,14 @@ SELECT 'A' >'a';
 SELECT 21>12;
 SELECT -1<-9;
 SELECT title FROM books WHERE released_year<2000;
+
+
+--  AND (&&)
+
+
+SELECT title FROM books WHERE author_fname ='dave' && author_lname='eggers' && released_year>2010;
+SELECT -10>-20 AND 0<=0;
+SELECT -40<=0 AND 10>40;
+SELECT 54<=54 && 'A'='a';
+SELECT title,author_lname FROM books where  title LIKE "%novel" AND author_lname='eggers' AND released_year >2010;
+
