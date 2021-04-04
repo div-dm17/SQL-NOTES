f0e51eb63e280519f439349ac20fc99d232847b0
Divyanshu Mishra <37334916+div-dm17@users.noreply.github.com>
2021-04-04 20:43:29 +0530

    Adding G.T LESS THAN
    query on >, >= , < <=

--- BOOK./Logical.sql
+++ BOOK./Logical.sql
@@ -1,3 +1,11 @@
 
 SELECT released_year FROM books WHERE released_year =2017;
 SELECT released_year FROM books WHERE released_year !=2017;
+
+SELECT title FROM books WHERE title NOT LIKE '%W ';
+SELECT * FROM books WHERE released_year >2000;
+SELECT author_fname ,author_lname , title FROM books WHERE pages >400;
+SELECT 'A' >'a';
+SELECT 21>12;
+SELECT -1<-9;
+SELECT title FROM books WHERE released_year<2000;
