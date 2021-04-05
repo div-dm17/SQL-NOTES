859e87a0aed0ebbd8f8b6381bf6fe387117bd336
Divyanshu Mishra <37334916+div-dm17@users.noreply.github.com>
2021-04-05 08:06:17 +0530

    Added between
    NOT BETWEEN , IN

--- BOOK./Logical.sql
+++ BOOK./Logical.sql
@@ -19,5 +19,20 @@ SELECT -10>-20 AND 0<=0;
 SELECT -40<=0 AND 10>40;
 SELECT 54<=54 && 'A'='a';
 SELECT title,author_lname FROM books where  title LIKE "%novel" AND author_lname='eggers' AND released_year >2010;
-
+-- or 
 SELECT title,author_lname, released_year FROM books WHERE released_year>2010 || author_lname='eggers' || stock_quantity=100;
+SELECT *  FROM books WHERE released_year>+2004  && released_year<=2015;
+
+
+-- between not between in
+SELECT * FROM books WHERE released_year BETWEEN 2004 AND 2015;
+SELECT * FROM books WHERE released_year NOT BETWEEN 2004 AND 2015;
+SELECT * FROM books WHERE author_lname IN ('carver' , 'smith' , 'lahiri');
+
+SELECT * FROM books WHERE released_year NOT IN (2000,2002,2004,2006,2008,2010,2012,2014,2016);
+SELECT * FROM books WHERE released_year %2=0 AND released_year>=2000;
+SELECT * FROM books WHERE released_year %2!=0 AND released_year NOT IN (2000,2002,2004,2006,2008,2010,2012,2014,2016);
+
+
+
+--
