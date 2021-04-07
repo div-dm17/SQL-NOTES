0fdf6ce58f50154f7898bf58915880b2ca9e56fd
Divyanshu Mishra <37334916+div-dm17@users.noreply.github.com>
2021-04-07 06:43:09 +0530

    ADDED EXPLICIT INNER JOIN

--- Advance./run.sql
+++ Advance./run.sql
@@ -13,3 +13,14 @@ WHERE CUSTOMERS.id  =  customer_id;
 SELECT first_name, last_name, email , amount , order_date 
 FROM CUSTOMERS, ORDERS
 WHERE CUSTOMERS.id = ORDERS.customer_id;
+
+-- EXPLICIT INNER JOIN
+SELECT * FROM CUSTOMERS
+JOIN 
+ORDERS 
+ON CUSTOMERS.id = ORDERS.customer_id;
+SELECT first_name, last_name, order_date, amount  FROM
+CUSTOMERS
+JOIN 
+ORDERS
+ON CUSTOMERS.id = ORDERS.customer_id;
